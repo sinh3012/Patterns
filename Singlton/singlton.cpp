eb4cc4be7c4c1ecad086292dc50f3daa6ed14d13
Lair Sinh <assassin3012@mail.ru>
2016-12-16 01:48:16 +0300

    Create singlton.cpp

---
+++ Singlton/singlton.cpp
@@ -0,0 +1,14 @@
+class Singleton
+{
+public:
+  static Singleton& Instance()
+    {
+      static Singleton s;
+      return s;
+    }
+private:
+  Singleton() { ... } 
+  ~Singleton() { ... }
+  Singleton(Singleton const&) = delete;
+  Singleton& operator= (Singleton const&) = delete;
+};
